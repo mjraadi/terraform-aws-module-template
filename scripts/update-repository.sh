4a4240af09c76b5465065bce437278ef2359e3e6
Rohith Jayawardene <gambol99@gmail.com>
2024-11-14 13:08:50 +0000

    chore: adding the init

---
+++ scripts/update-repository.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SCM=${HOME}/scm
+APPVIA_GITHUB=${SCM}/github.com/appvia
+TEMPLATE_AWS_MODULE=${APPVIA_GITHUB}/terraform-aws-module-template
+
+/bin/cp ${TEMPLATE_AWS_MODULE}/Makefile .
+/bin/cp ${TEMPLATE_AWS_MODULE}/.gitignore .
+/bin/cp ${TEMPLATE_AWS_MODULE}/.tflint.hcl .
+/bin/cp ${TEMPLATE_AWS_MODULE}/.terraform-docs.yml .
+/bin/cp ${TEMPLATE_AWS_MODULE}/.commitlintrc.yaml .
+/bin/cp ${TEMPLATE_AWS_MODULE}/SECURITY.md .
+/bin/cp ${TEMPLATE_AWS_MODULE}/CODE_OF_CONDUCT.md .
+
+if [[ -f ".terraform-docs.yaml" ]]; then
+  rm -f .terraform-docs.yaml
+fi
